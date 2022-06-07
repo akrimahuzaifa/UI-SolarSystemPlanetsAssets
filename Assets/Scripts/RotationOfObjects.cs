6ad05915e535dc7284fc3c03cfe03353e3d6e09d
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2022-06-07 10:16:25 +0500

    InitialCommit-First

---
+++ Assets/Scripts/RotationOfObjects.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationOfObjects : MonoBehaviour
+{
+/*    public float speed = 20f;
+
+    GameObject cube0, cube1, sphere0;
+
+    private void Start()
+    {
+        cube0 = GameObject.Find("Cube0");
+        cube1 = GameObject.Find("Cube1");
+        sphere0 = GameObject.Find("Sphere0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //cube0.transform.rotation = Quaternion.Euler(cube0.transform.rotation.x, speed * Time.deltaTime, cube0.transform.rotation.z, Space.World);
+        cube0.transform.Rotate(0, speed * Time.deltaTime, 0, Space.World);
+        cube1.transform.Rotate(0, speed * Time.deltaTime, 0, Space.World);
+        sphere0.transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }*/
+}
---
+++ Assets/Scripts/VolumeOfObjects.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeOfObjects : MonoBehaviour
+{
+/*    public float volume = 1f;
+    GameObject cube0, cube1, sphere0;
+
+    private void Start()
+    {
+        cube0 = GameObject.Find("Cube0");
+        cube1 = GameObject.Find("Cube1");
+        sphere0 = GameObject.Find("Sphere0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube0.transform.localScale = new Vector3(volume, volume, volume);
+
+        //cube0.transform.localScale(volume, volume, volume);
+
+        *//*        cube0.transform.Rotate(0, volume * Time.deltaTime, 0);
+                cube1.transform.Rotate(0, volume * Time.deltaTime, 0);
+                sphere0.transform.Rotate(0, volume * Time.deltaTime, 0);*//*
+    }
+    public void AdjustVolume(float newVolume)
+    {
+        volume = newVolume;
+    }*/
+}
