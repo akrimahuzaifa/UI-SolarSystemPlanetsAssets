2aaa2dce44801052f5d3fecd79fbb62ff0cb34b6
Akrima Huzaifa Akhtar <akrimaamirka@gmail.com>
2022-06-07 10:42:55 +0500

    Working Sliders for Planets

--- Assets/Scripts/VolumeOfObjects.cs
+++ Assets/Scripts/VolumeOfObjects.cs
@@ -4,29 +4,25 @@ using UnityEngine;
 
 public class VolumeOfObjects : MonoBehaviour
 {
-/*    public float volume = 1f;
-    GameObject cube0, cube1, sphere0;
+    public float volume = 500f;
+    GameObject sun, earth, jupiter;
 
     private void Start()
     {
-        cube0 = GameObject.Find("Cube0");
-        cube1 = GameObject.Find("Cube1");
-        sphere0 = GameObject.Find("Sphere0");
+        sun = GameObject.Find("Sun");
+        earth = GameObject.Find("Earth");
+        jupiter = GameObject.Find("Jupiter");
     }
 
     // Update is called once per frame
     void Update()
     {
-        cube0.transform.localScale = new Vector3(volume, volume, volume);
+        sun.transform.localScale = new Vector3(volume, volume, volume);
 
         //cube0.transform.localScale(volume, volume, volume);
-
-        *//*        cube0.transform.Rotate(0, volume * Time.deltaTime, 0);
-                cube1.transform.Rotate(0, volume * Time.deltaTime, 0);
-                sphere0.transform.Rotate(0, volume * Time.deltaTime, 0);*//*
     }
     public void AdjustVolume(float newVolume)
     {
         volume = newVolume;
-    }*/
+    }
 }
